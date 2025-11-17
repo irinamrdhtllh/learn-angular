65042283e34620928d1293fce4b0246ccf7dd50a
Irina M. S <irinamardhatillah@gmail.com>
2025-11-17 15:17:43 +0700

    Add routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,27 @@
-import {Component} from '@angular/core';
-import { Home } from './home/home';
+import { Component } from "@angular/core";
+import { Home } from "./home/home";
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [Home],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
-  styleUrls: ['./app.css'],
+    selector: "app-root",
+    imports: [Home, RouterOutlet, RouterLink],
+    template: `
+        <main>
+            <header class="brand-name">
+                <img
+                    class="brand-logo"
+                    src="/assets/logo.svg"
+                    alt="logo"
+                    aria-hidden="true"
+                />
+            </header>
+            <section class="content">
+                <router-outlet />
+            </section>
+        </main>
+    `,
+    styleUrls: ["./app.css"],
 })
 export class App {
-  title = 'default';
+    title = "default";
 }
